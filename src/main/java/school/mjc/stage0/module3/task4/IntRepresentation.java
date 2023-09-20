30a322cdb630c731b0b3773fc37b4a264a3d2bb6
olzhasnew <olzhas.ualikhanov@gmail.com>
2023-09-20 19:30:38 +0600

    edit

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.module3.task4;
 public class IntRepresentation {
     public static void main(String[] args) {
         int c ='c';
-        int h ='h';
+        int h = 'h';
         int a = 'a';
         int r = 'r';
         System.out.println(c);
