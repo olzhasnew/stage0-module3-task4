e09672bd41f565571f6cb3d2a6c55ce030c56a21
olzhasnew <olzhas.ualikhanov@gmail.com>
2023-09-20 19:43:13 +0600

    trying anew

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -5,5 +5,7 @@ public class FormulaCreator {
         int a = 0;
         int b = 0;
         int formula = (9*a*a - 5*b + 2 + a - 7)*((a+b-4*a*b)/2);
+
+        System.out.println(formula);
     }
 }
